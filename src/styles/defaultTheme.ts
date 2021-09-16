3a760a7b3c407e9c1430a372ad40454278abf868
Guilherme Brito <guilhermebritto.prof@gmail.com>
2021-09-16 17:07:02 -0300

    Implementação do thema default na aplicação

--- src/styles/defaultTheme.ts
+++ src/styles/defaultTheme.ts
@@ -2,6 +2,27 @@ import {DefaultTheme} from 'styled-components'
 
 
 export const defaultTheme: DefaultTheme = {
-  
-
+  colors:{
+    primary: '#2B52DD',
+    secondary: '#004282 ',
+    neutral:{
+      white: "#FFF",
+      light:  "#F4F6FC",
+      soft:   "#A7ADBF",
+      medium: "#6D7384",
+      dark :  "#2B2C32",
+    },
+    suport: {
+      success: "#3EB96F",
+      alert: "#FBC436",
+      error: "#E73A33",
+    }
+  },
+  borderRadius: "4px",
+  fontSizes:{
+    small: "12px",
+    medium: "14px",
+    large: "16px",
+    xlarge: "18px",
+  }
 }
