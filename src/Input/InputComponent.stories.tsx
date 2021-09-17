85c204545da5d1160a10a61b20ed0189c88f521c
Vitor Hugo <38621315+includeVitor@users.noreply.github.com>
2021-09-17 01:05:46 -0300

    Add general configs (#10)
    * add README.md
    
    * add IconSide to storybook
    
    * add prettier & editor config
    
    * yarn format the app
    
    * add type-check to main
    
    * add Buttom && Input

--- src/Input/InputComponent.stories.tsx
+++ src/Input/InputComponent.stories.tsx
@@ -1,43 +1,43 @@
-import React from "react";
-import InputComponent from "./InputComponent";
+import React from 'react'
+import InputComponent from './InputComponent'
 
 export default {
-  title: "Input",
-  component: InputComponent,
-  decorators: [
-    (Story) => (
-      <div style={{ width: "200px" }}>
-        <Story />
-      </div>
-    ),
-  ],
-  argTypes: {
-    variant: {
-      options: ["text", "password"],
-      control: {
-        type: "radio",
-      },
+    title: 'Input',
+    component: InputComponent,
+    decorators: [
+        Story => (
+            <div style={{ width: '200px' }}>
+                <Story />
+            </div>
+        )
+    ],
+    argTypes: {
+        variant: {
+            options: ['text', 'password'],
+            control: {
+                type: 'radio'
+            }
+        },
+        value: {
+            control: {
+                type: 'text'
+            }
+        },
+        label: {
+            control: {
+                type: 'text'
+            }
+        },
+        error: {
+            control: {
+                type: 'object'
+            }
+        }
     },
-    value: {
-      control: {
-        type: "text",
-      },
-    },
-    label: {
-      control: {
-        type: "text",
-      },
-    },
-    error: {
-      control: {
-        type: "object",
-      },
-    },
-  },
-  arg: {
-    variant: "text",
-    label: "Label",
-  },
-};
+    arg: {
+        variant: 'text',
+        label: 'Label'
+    }
+}
 
-export const Input = (args) => <InputComponent {...args} />;
+export const Input = args => <InputComponent {...args} />
--- src/lib/test-utils.tsx
+++ src/lib/test-utils.tsx
@@ -1,20 +1,20 @@
-import { render, RenderOptions } from "@testing-library/react";
-import React, { ReactElement, FC } from "react";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "../styles/defaultTheme";
+import { render, RenderOptions } from '@testing-library/react'
+import React, { ReactElement, FC } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/defaultTheme'
 
 const Providers: FC = ({ children }) => {
-  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
-};
+    return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+}
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 const customRender = (
-  ui: ReactElement,
-  options?: Omit<RenderOptions, "queries">
-) => render(ui, { wrapper: Providers, ...options });
+    ui: ReactElement,
+    options?: Omit<RenderOptions, 'queries'>
+) => render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
-export * from "@testing-library/react";
+export * from '@testing-library/react'
 
 // override render method
-export { customRender as render };
+export { customRender as render }
