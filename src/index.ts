155f6dddabe0ac297a764ec601848eda1ddfe5f3
Guilherme Brito <guilhermebritto.prof@gmail.com>
2021-09-24 09:31:25 -0300

    Correção de exportação do useApi e use Form

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import Button from "@components/Button/ButtonComponent";
 import {defaultTheme} from "./styles/defaultTheme";
 import { CustomThemeProvider, useTheme } from "./hooks/ThemeContext";
 import { createTheme } from "@lib/createTheme";
-import { useApi } from '@hooks/useApi/index'
-import { useForm, FormStatus } from '@hooks/useForm/index'
+import { useApi } from './hooks/useApi/index'
+import { useForm, FormStatus } from './hooks/useForm/index'
 
 export {
   Input,
