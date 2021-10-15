85fffbc78a60d292dd69810642f5eb6cfb271cce
Guilherme Brito <guilhermebritto.prof@gmail.com>
2021-10-15 12:57:42 -0300

    Adição de componente de swtich (#19)
    * Adição de componente de swtich
    
    * Adição de testes para aumentar a cobertura do código
    
    * Remoção de linhas que estão em lugares errados

---
+++ src/components/Switch/__tests__/SwitchComponent.tsx
@@ -0,0 +1,60 @@
+import { fireEvent, render, screen } from '../../../lib/test-utils'
+
+import SwitchComponent from '../SwitchComponent'
+
+describe('<SwitchComponent />', () => {
+    it('Should be able to click and change state from not checked to checked', () => {
+        let checked = false
+        const setChecked = (check: boolean) => (checked = check)
+
+        const { getByTestId } = render(
+            <SwitchComponent isChecked={checked} setIsChecked={setChecked} />
+        )
+
+        const switchButton = getByTestId('switch-button')
+
+        fireEvent.click(switchButton)
+
+        expect(checked).toBe(true)
+    })
+
+    it('Should be able to render the switch buttom with pressed Down and show aura', () => {
+        const { getByTestId } = render(<SwitchComponent isChecked={false} />)
+
+        const switchButtonAura = getByTestId('switch-button-aura')
+
+        fireEvent.mouseDown(switchButtonAura)
+
+        expect(switchButtonAura).toMatchSnapshot()
+    })
+
+    it('Should be able to render the switch buttom with pressed Up and show aura', () => {
+        const { getByTestId } = render(<SwitchComponent isChecked={false} />)
+
+        const switchButtonAura = getByTestId('switch-button-aura')
+
+        fireEvent.mouseUp(switchButtonAura)
+
+        expect(switchButtonAura).toMatchSnapshot()
+    })
+
+    it('Should be able to render a not active switch', () => {
+        const { container } = render(<SwitchComponent isChecked={false} />)
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('Should be able to render a active switch', () => {
+        const { container } = render(<SwitchComponent isChecked={true} />)
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('Should be able to render disabled switch', () => {
+        const { container } = render(
+            <SwitchComponent isChecked={false} disabled />
+        )
+
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
