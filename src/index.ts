af26bde5506d529654ce66414c2710d3cc57d899
bruno.tomaz <bruno.tomaz@dtidigital.com.br>
2021-10-27 16:53:34 -0300

    feat: add stat card

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /** Componentes */
 import Input from "./components/Input/InputComponent";
 import Button from "./components/Button/ButtonComponent";
+import StatCard from "@components/StatCard/StatCardComponent";
 import Tag from './components/Tag/TagComponent'
 import Switch from './components/Switch/SwitchComponent'
 import Checkbox from './components/Checkbox/CheckboxComponent'
@@ -17,6 +18,7 @@ import { CustomThemeProvider, useTheme } from "./hooks/useTheme";
 export {
   Input,
   Button,
+  StatCard,
   Tab,
   TabPanel,
   ToastContainer,
