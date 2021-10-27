bbf49b767b6ed4038714c5f70a0c00f3136b3f45
bruno.tomaz <bruno.tomaz@dtidigital.com.br>
2021-10-27 16:39:47 -0300

    feat: add stat card

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import Input from '@components/Input/InputComponent'
 import Button from '@components/Button/ButtonComponent'
+import StatCard from '@components/StatCard/StatCardComponent'
+import ToastContainer from '@components/ToastContainer/ToastComponent'
 import { defaultTheme } from './styles/defaultTheme'
-import { CustomThemeProvider, useTheme } from './hooks/useTheme'
 import { createTheme } from '@lib/createTheme'
+import { CustomThemeProvider, useTheme } from './hooks/useTheme'
 import { useApi } from './hooks/useApi/index'
 import { useForm, FormStatus } from './hooks/useForm/index'
 import { ToastProvider, useToast } from './hooks/useToast'
-import ToastContainer from './components/ToastContainer/ToastComponent'
 
 export {
     Input,
     Button,
+    StatCard,
     ToastContainer,
     ToastProvider,
     useToast,
