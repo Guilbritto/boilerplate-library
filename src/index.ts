572d794439a1679e54917d947cfdd8e7b8082bb2
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-10-28 13:28:01 -0300

    fixing export StatCard

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /** Componentes */
 import Input from './components/Input/InputComponent'
 import Button from './components/Button/ButtonComponent'
-import StatCard from '@components/StatCard/StatCardComponent'
+import StatCard from './components/StatCard/StatCardComponent'
 import Tag from './components/Tag/TagComponent'
 import Switch from './components/Switch/SwitchComponent'
 import Checkbox from './components/Checkbox/CheckboxComponent'
