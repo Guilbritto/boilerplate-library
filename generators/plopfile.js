3b88038b2698a059b1a190f2fa3f4ba6c02a5f52
bruno.tomaz <bruno.tomaz@dtidigital.com.br>
2021-12-17 11:42:32 -0300

    refactor: format file

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -11,32 +11,27 @@ module.exports = plop => {
         actions: [
             {
                 type: 'add',
-                path:
-                    '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.tsx',
+                path: '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.tsx',
                 templateFile: 'templates/Component.tsx.hbs'
             },
             {
                 type: 'add',
-                path:
-                    '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.styles.ts',
+                path: '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.styles.ts',
                 templateFile: 'templates/styles.ts.hbs'
             },
             {
                 type: 'add',
-                path:
-                    '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.stories.tsx',
+                path: '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.stories.tsx',
                 templateFile: 'templates/stories.tsx.hbs'
             },
             {
                 type: 'add',
-                path:
-                    '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.types.ts',
+                path: '../src/components/{{pascalCase name}}/{{pascalCase name}}Component.types.ts',
                 templateFile: 'templates/types.ts.hbs'
             },
             {
                 type: 'add',
-                path:
-                    '../src/components/{{pascalCase name}}/__tests__/{{pascalCase name}}Component.tsx',
+                path: '../src/components/{{pascalCase name}}/__tests__/{{pascalCase name}}Component.tsx',
                 templateFile: 'templates/test.tsx.hbs'
             }
         ]
